a5bdb82e4a205a0f2c83bdd5f66494d99fb12f30
*allan9595* <bohanzhang@my.unt.edu>
2019-03-21 00:01:27 -0500

    edit files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const methodOverride = require('method-override');
-
+const path = require('path');
 const users = require('./routes/api/users');
 const project = require('./routes/api/project');
 const team = require('./routes/api/team');
