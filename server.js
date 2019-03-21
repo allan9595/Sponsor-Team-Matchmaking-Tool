43e662a4681ed0ab55b5d791b6f51f806f2f5cb7
*allan9595* <bohanzhang@my.unt.edu>
2019-03-21 00:07:56 -0500

    edit files

--- server.js
+++ server.js
@@ -23,15 +23,6 @@ app.use(methodOverride('_method'));
 const db = require('./config/keys').mongoURI;
 
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 //connect db
 
@@ -51,6 +42,18 @@ require('./config/passport')(passport);
 app.use('/api/users',users);
 app.use('/api/project',project);
 app.use('/api/team',team);
+
+
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
